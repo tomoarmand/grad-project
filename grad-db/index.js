57bcf47a61e4840f7b440a4c4097f726fb40aec8
tomoarmand <tomo.armand@gmail.com>
2025-05-31 19:38:42 +1200

    Added code to prepare for Render

--- grad-db/index.js
+++ grad-db/index.js
@@ -57,6 +57,8 @@ app.delete('/exercises/:id', async (req, res) => {
     res.json(remainingExercises);
 })
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
