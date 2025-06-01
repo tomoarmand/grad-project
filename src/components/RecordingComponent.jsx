53002965ddaa33b6098fd6d760f0a4b985683e74
tomoarmand <tomo.armand@gmail.com>
2025-06-01 19:12:50 +1200

    m

--- src/components/RecordingComponent.jsx
+++ src/components/RecordingComponent.jsx
@@ -12,7 +12,14 @@ function RecordingComponent({ onSave }) {
         const stream = await navigator.mediaDevices.getUserMedia({ audio : true });
         // Asks the browser for permission to use microphone
         // Stream is an audio stream from the mic
-        mediaRecorderRef.current = new MediaRecorder(stream)
+
+        const mimeType = 'audio/webm;codecs=opus';
+            if (!MediaRecorder.isTypeSupported(mimeType)) {
+                alert("⚠️ Audio format not supported on this device.");
+                return;
+            };
+
+        mediaRecorderRef.current = new MediaRecorder(stream, { mimeType });
         // Creates a new MediaRecorder object to handle recording
         audioChunks.current = [];
         // Clears any previous chunks
@@ -24,9 +31,12 @@ function RecordingComponent({ onSave }) {
         };
 
         mediaRecorderRef.current.onstop = () => {
-            const blob = new Blob(audioChunks.current, { type: "audio/webm" });
+            const blob = new Blob(audioChunks.current, { type: mimeType });
             // Combines all the audio chunks into a single Blob, a binary object representing the audio file
-            
+            if (blob.size === 0) {
+                alert("❌ Recording failed or was too short. Please try again.");
+                return;
+            };
             const reader = new FileReader();
             // Uses a Filereader to convert the blob to a base64 string
             // This format is useful for sending audio to a server or storing it in a databse
