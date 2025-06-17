ff8ddd9750722154c2be95eee283424fbd07d983
tomoarmand <tomo.armand@gmail.com>
2025-06-17 15:28:57 +1200

    add zustand persist

--- src/store/userStore.js
+++ src/store/userStore.js
@@ -1,9 +1,17 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
-const useUserStore = create(set => ({
-  user: null,
-  setUser: (user) => set({ user }),
-  clearUser: () => set({ user: null }),
-}));
+const useUserStore = create(
+  persist(
+    (set) => ({
+      user: null,
+      setUser: (user) => set({ user }),
+      clearUser: () => set({ user: null }),
+    }),
+    {
+      name: 'user-storage', // key in localStorage
+    }
+  )
+);
 
 export default useUserStore;
